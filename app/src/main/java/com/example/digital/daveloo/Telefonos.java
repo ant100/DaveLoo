2f59805da80a9b593f35ff0e81c2eef4390a6a90
Sebastian Davelouis <seplidavelouis@yahoo.com>
2018-08-30 12:00:51 -0500

    telefono agregado
    Agregado fragmento para telefono y llenado de telefonos de emergencia. Falta perfeccionar el layout. No sé cómo pegar el ícono del teléfono a la derecha aún.

--- app/src/main/java/com/example/digital/daveloo/Telefonos.java
+++ app/src/main/java/com/example/digital/daveloo/Telefonos.java
@@ -5,12 +5,54 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Telefonos extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_telefono, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_telefono, container, false);
+
+        String[] from = new String[] { "name", "desc" };
+        int[] to = new int[] { R.id.contacto, R.id.numero };
+
+        ArrayList<String[]> lista_telf = new ArrayList<String[]>();
+
+        String[] evento1 = {"Policía", "105", "1" };
+        String[] evento2 = {"Bomberos", "116", "2" };
+        String[] evento3 = {"SAMU", "106", "3" };
+        String[] evento4 = {"Cruz Roja", "115", "3" };
+        String[] evento5 = {"Defensa Civil", "110", "3" };
+        String[] evento6 = {"Emergencia ESSALUD", "117", "3" };
+
+        lista_telf.add(evento1);
+        lista_telf.add(evento2);
+        lista_telf.add(evento3);
+        lista_telf.add(evento4);
+        lista_telf.add(evento5);
+        lista_telf.add(evento6);
+
+        ArrayList<HashMap<String, String>> eventos = new ArrayList<HashMap<String, String>>();
+
+        for (String[] evento : lista_telf) {
+            HashMap<String, String> datosEvento = new HashMap<String, String>();
+
+            datosEvento.put("name", evento[0]);
+            datosEvento.put("desc", evento[1]);
+            datosEvento.put("id", evento[2]);
+
+            eventos.add(datosEvento);
+        }
+
+        SimpleAdapter listadoAdapter = new SimpleAdapter(getContext(), eventos, R.layout.fragment_telefono_fila, from, to);
+
+        ListView lv = (ListView)rootView.findViewById(R.id.lista2);
+        lv.setAdapter(listadoAdapter);
+        return rootView;
     }
 }
