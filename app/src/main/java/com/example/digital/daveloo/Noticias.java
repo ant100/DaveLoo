e462e88e067407bf4af49f812433d2d22ad83bbd
Ann <antuanette.loo@gmail.com>
2018-09-18 12:57:01 -0500

    web service Noticias

--- app/src/main/java/com/example/digital/daveloo/Noticias.java
+++ app/src/main/java/com/example/digital/daveloo/Noticias.java
@@ -14,6 +14,21 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class Noticias extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,38 +36,71 @@ public class Noticias extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_noticias, container, false);
 
-        String[] from = new String[] { "titulo", "id" };
-        int[] to = new int[] { R.id.titulo, R.id.noticia_id };
-
-        ArrayList<String[]> lista = new ArrayList<String[]>();
-
-        String[] evento1 = {"Transferencia de gestión administrativa", "1" };
-        String[] evento2 = {"A comer sano en Miraflores", "2" };
-        String[] evento3 = {"Sistema de bicicleta pública", "3" };
-        String[] evento4 = {"Subasta pública de siete módulos comerciales", "3" };
-        String[] evento5 = {"Contribuyentes no habidos", "3" };
-
-        lista.add(evento1);
-        lista.add(evento2);
-        lista.add(evento3);
-        lista.add(evento4);
-        lista.add(evento5);
-
-        ArrayList<HashMap<String, String>> eventos = new ArrayList<HashMap<String, String>>();
-
-        for (String[] evento : lista) {
-            HashMap<String, String> datosEvento = new HashMap<String, String>();
-
-            datosEvento.put("titulo", evento[0]);
-            datosEvento.put("id", evento[1]);
-
-            eventos.add(datosEvento);
-        }
-
-        SimpleAdapter listadoAdapter = new SimpleAdapter(getContext(), eventos, R.layout.fragment_noticias_fila, from, to);
-
         ListView lv = (ListView)rootView.findViewById(R.id.lista1);
-        lv.setAdapter(listadoAdapter);
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://daveloo.000webhostapp.com/notas")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Unexpected code " + response);
+                } else {
+                    // conseguir la cadena json
+                    String cadenaJson = response.body().string();
+                    Log.i("====>", cadenaJson);
+
+                    Gson gson = new Gson();
+                    Type stringStringMap = new TypeToken<ArrayList<Map<String, Object>>>() { }.getType();
+
+                    final ArrayList<Map<String, Object>> retorno = gson.fromJson(cadenaJson, stringStringMap);
+                    final ListView listView = (ListView)getView().findViewById(R.id.lista1);
+
+                    final String[] from = new String[] { "noticia_id", "noticia_titular" };
+                    final int[] to = new int[] { R.id.noticia_id, R.id.titulo };
+
+                    final ArrayList<HashMap<String, String>> eventos = new ArrayList<HashMap<String, String>>();
+
+                    // pasar del Objeto retorno al arraylist Lista
+                    ArrayList<String[]> lista = new ArrayList<String[]>();
+                    int i = 0;
+                    for (Map<String, Object> x : retorno) {
+                        String[] eventoX = {"\""+x.get("noticia_id")+"\"", "\""+x.get("noticia_titular")+"\"" };
+                        lista.add(eventoX);
+                    }
+
+                    for (String[] evento : lista) {
+                        HashMap<String, String> datosEvento = new HashMap<String, String>();
+                        datosEvento.put("noticia_id", evento[0]);
+                        datosEvento.put("noticia_titular", evento[1]);
+                        eventos.add(datosEvento);
+                    }
+
+
+                    getActivity().runOnUiThread(new Runnable() {
+                        public void run() {
+
+                            SimpleAdapter listadoAdapter = new SimpleAdapter(getContext(), eventos, R.layout.fragment_noticias_fila, from, to);
+
+                            ListView lv = (ListView)getView().findViewById(R.id.lista1);
+                            lv.setAdapter(listadoAdapter);
+
+                        }
+                    });
+
+
+                }
+            }
+        });
+
 
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
